9dd2753cb7ef4c96f4cbc417910fbb6730a0664d
jgustavoj <gjime011@fiu.edu>
2021-02-18 20:26:12 -0500

    complete

--- myscript.sh
+++ myscript.sh
@@ -89,16 +89,60 @@ NAME="Bob"
 
 
 #CASE STATEMENT 
-read -p "Are you 21 or over? Y/N" ANSWER 
-case "$ANSWER" in 
-    [yY] | [yY][eE][sS])
-        echo "You can have a beer :)"
-        ;;
-    [nN] | [nN][oO])
-        echo "Sorry no drinking"
-        ;;
-    *)
-        echo "Please enter y/yes or n/no"
-        ;;
+# read -p "Are you 21 or over? Y/N" ANSWER 
+# case "$ANSWER" in 
+#     [yY] | [yY][eE][sS])
+#         echo "You can have a beer :)"
+#         ;;
+#     [nN] | [nN][oO])
+#         echo "Sorry no drinking"
+#         ;;
+#     *)
+#         echo "Please enter y/yes or n/no"
+#         ;;
     
-esac
+# esac
+
+#SIMPLE FOR LOOP
+# NAMES="Brad Kevin Alice Mark"
+# for NAME in $NAMES
+#     do 
+#         echo "Hello $NAME"
+# done 
+
+# FOR LOOP TO RENAME FILES
+# FILES=$(ls *.txt)
+# NEW="new"
+# for FILE in $FILES 
+#     do 
+#         echo "Renaming $FILES to new-$FILE"
+#         mv $FILE $NEW-$FILE
+# done
+
+
+# WHILE LOOP - READ THROUGH A FILE LINE BY LINE
+# LINE=1
+# while read -r CURRENT_LINE
+#     do 
+#         echo "$LINE: $CURRENT_LINE"
+#         ((LINE++))
+# done < "./new-1.txt"
+
+#FUNCTION
+
+# function sayHello(){
+#     echo "Hello World"
+# }
+# sayHello
+
+#FUNCTION WITH PARAMS
+# function greet(){
+#     echo "Hello, I am $1 and I am $2"
+# }
+# greet "Gus" "202"
+
+#CREATE FOLDER AND WRITE TO A FILE 
+mkdir hello
+touch "hello/world.txt"
+echo "Hello World" >> "hello/world.txt"
+echo "Created hello/world.txt"
